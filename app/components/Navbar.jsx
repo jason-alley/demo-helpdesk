8c123a27b99c646dba17dcc052ef98e9f598bc08
Jason Cameron <jasoncameron246@gmail.com>
2023-09-04 23:51:19 -0400

    navbar update to add logo image.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -1,13 +1,18 @@
 import Link from "next/link"
+import Image from "next/image"
+import Logo from "../../public/help-desk.png"
 
 export default function Navbar() {
   return (
     <nav>
-        <h2>Help Desk</h2>
-        <ul>
-            <li><Link href="/">Dashboard</Link></li>
-            <li><Link href="/tickets">Tickets</Link></li>
-        </ul>
+        <Image src={Logo}
+          width={60}
+          quality={100}
+          alt="Help Desk Logo"
+          placeholder="blur"
+        />
+        <Link href="/">Dashboard</Link>
+        <Link href="/tickets">Tickets</Link>
     </nav>
   )
 }
