a845cbb86bbd73fa86f254156496defff69a1438
Jason Cameron <jasoncameron246@gmail.com>
2023-09-06 16:55:06 -0400

    added ticket create feature.

---
+++ app/tickets/create/page.jsx
@@ -0,0 +1,11 @@
+// Components
+import CreateForm from "@/app/components/CreateForm";
+
+export default function CreateTicket() {
+  return (
+    <main>
+        <h2 className="text-primary text-center">Add a new ticket.</h2>
+        <CreateForm />
+    </main>
+  )
+}
