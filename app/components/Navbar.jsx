f538317d3c49863da10189dca87902284c2c4590
Jason Cameron <jasoncameron246@gmail.com>
2023-09-07 12:16:40 -0400

    more button component refactor.

--- app/components/Navbar.jsx
+++ app/components/Navbar.jsx
@@ -2,6 +2,9 @@ import Link from "next/link"
 import Image from "next/image"
 import Logo from "../../public/help-desk.png"
 
+//components
+import Button from "./Button"
+
 export default function Navbar() {
   return (
     <nav>
@@ -13,6 +16,13 @@ export default function Navbar() {
         />
         <Link href="/">Dashboard</Link>
         <Link href="/tickets">Tickets</Link>
+        <Button 
+          text="View Tickets"
+          link="/tickets"
+          linkClasses="ml-auto mr-8"
+          classes="btn-primary rounded-md"
+        />
+
     </nav>
   )
 }
--- app/page.jsx
+++ app/page.jsx
@@ -10,14 +10,6 @@ export default function Home() {
       <h2>Dashboard</h2>
       <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Vero repellendus tempore, exercitationem odit, quasi doloremque possimus recusandae alias sequi totam soluta natus iure eius, obcaecati sint dolores blanditiis aspernatur quo officia iusto ut. Et, aliquid sed voluptates iste cum totam, facere explicabo, fugit suscipit ratione aspernatur consequuntur ex mollitia quaerat?</p>
 
-      <div className="flex justify-center my-8">
-        <Button 
-          text="View Tickets"
-          link="/tickets"
-          classes="btn-primary"
-        />
-      </div>
-
       <h2>Company Updates</h2>
 
       <div className="card">
